e15dd7fa7aa9bf7575bf39f42f59cb75fb896edb
ozaki <fs65ugumori@yahoo.co.jp>
2018-01-29 13:28:29 +0900

    テストを作成

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { app } from 'hyperapp';
 import state from './state';
 import actions from './actions';
-import view from './components/counter';
+import view from './components/Counter';
 import './main.css';
 
 app(state, actions, view, document.body);
---
+++ test/counter.test.js
@@ -0,0 +1,62 @@
+import { h, app } from 'hyperapp';
+import appActions from '../src/actions';
+import appState from '../src/state';
+import Counter from '../src/components/Counter';
+
+beforeEach(() => {
+  document.body.innerHTML = '';
+});
+
+describe('カウンター', () => {
+  test('初期表示は0であること', (done) => {
+    const view = (state, actions) =>
+      h(
+        'main',
+        {
+          oncreate() {
+            expect(document.body.innerHTML.includes('0')).toBe(true);
+            done();
+          },
+        },
+        [Counter(state, actions)],
+      );
+
+    app(appState, appActions, view, document.body);
+  });
+
+  test('+を押すと1になること', (done) => {
+    const view = (state, actions) =>
+      h(
+        'div',
+        {
+          oncreate() {
+            expect(document.body.innerHTML.includes('1')).toBe(true);
+            done();
+          },
+        },
+        [Counter(state, actions)],
+      );
+
+    const main = app(appState, appActions, view, document.body);
+
+    main.up();
+  });
+
+  test('-を押すと-1になること', (done) => {
+    const view = (state, actions) =>
+      h(
+        'div',
+        {
+          oncreate() {
+            expect(document.body.innerHTML.includes('-1')).toBe(true);
+            done();
+          },
+        },
+        [Counter(state, actions)],
+      );
+
+    const main = app(appState, appActions, view, document.body);
+
+    main.down();
+  });
+});
